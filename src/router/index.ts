c2fc86c7a88c258e6caef45230d71e8fa54b8bdd
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-05 10:31:37 +0330

    Add TheGroups view and update routing
    Introduces a new TheGroups.vue view displaying a list of users. Updates the router to set TheGroups as the root path and moves the login view to '/login'. Also renames the store file from counter.ts to group.store.ts.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheLogin from '@/views/TheLogin.vue'
+import TheGroups from '@/views/TheGroups.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      component: TheGroups,
+    },
+    {
+      path: '/login',
       component: TheLogin,
     },
   ],
