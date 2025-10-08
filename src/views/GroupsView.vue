110fae366a2af167dc4f7be0907f2c8428c66064
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-08 13:19:23 +0330

    Update GroupsView.vue

--- src/views/GroupsView.vue
+++ src/views/GroupsView.vue
@@ -55,14 +55,13 @@ const groups = ref<IGroup[]>([
 
 <template>
   <UPageList divide>
-    <template v-for="(group, index) in groups" :key="index">
+    <template v-for="group in groups" :key="group.id">
       <UPageCard variant="ghost" :to="`/groups/${group.id}`">
         <template #body>
           <UUser :name="group.name" :description="group.description" :avatar="group.avatar" :to="`/groups/${group.id}`"
             size="xl" />
-          </template>
-        </UPageCard>
+        </template>
+      </UPageCard>
     </template>
-
   </UPageList>
 </template>
