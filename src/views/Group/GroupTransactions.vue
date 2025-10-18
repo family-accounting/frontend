fc3105fdad6a6b786db6dcb9c8eecc16dd864374
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-19 00:17:59 +0330

    Update GroupTransactions.vue

--- src/views/Group/GroupTransactions.vue
+++ src/views/Group/GroupTransactions.vue
@@ -1,9 +1,69 @@
 <template>
     <IonPage>
-        hjkhj
+        <IonHeader>
+            <IonToolbar>
+                <IonAvatar aria-hidden="true" slot="start">
+                    <img src="/logo.svg" />
+                </IonAvatar>
+                <IonTitle slot="start">
+                    Family Accounting
+                </IonTitle>
+
+            </IonToolbar>
+        </IonHeader>
+        <IonContent :fullscreen="true" :scroll-y="true">
+            <IonRefresher slot="fixed" @ionRefresh="handleRefresh($event)">
+                <IonRefresherContent></IonRefresherContent>
+            </IonRefresher>
+            <IonList>
+                <IonItem :detail="true" button :routerLink="`/transactions/${transaction.id}`"
+                    v-for="transaction in transactions" :key="transaction.id">
+                    <IonIcon aria-hidden="true" :icon="transaction.icon" slot="start"></IonIcon>
+                    <IonLabel>
+                        <h2>{{ transaction.name }}</h2>
+                    </IonLabel>
+                </IonItem>
+            </IonList>
+            <IonFab vertical="bottom" horizontal="start" slot="fixed">
+                <IonFabButton routerLink="/transactions/new">
+                    <IonIcon :icon="add"></IonIcon>
+                </IonFabButton>
+            </IonFab>
+        </IonContent>
     </IonPage>
 </template>
-<script setup lang="ts">
-import { IonPage } from '@ionic/vue';
 
+<script setup lang="ts">
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonAvatar,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    IonRefresher,
+    IonRefresherContent,
+    RefresherCustomEvent,
+} from '@ionic/vue';
+import { person, people, business, add } from 'ionicons/icons';
+import { ref } from 'vue';
+const transactions = ref([
+    { id: 1, name: 'Transaction 1', icon: person, },
+    { id: 2, name: 'Transaction 2', icon: people, },
+    { id: 3, name: 'Transaction 3', icon: business, },
+    { id: 4, name: 'Transaction 4', icon: add, },
+]);
+
+const handleRefresh = (event: RefresherCustomEvent) => {
+    setTimeout(() => {
+        // Any calls to load data go here
+        event.target.complete();
+    }, 2000);
+};
 </script>
