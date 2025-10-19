4e2ee10014713080d2499618096eeca5b988ad85
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-19 15:28:25 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,12 @@
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from "@ionic/vue";
-import { useTranslation } from "i18next-vue";
-import { useHead } from "@vueuse/head";
-import { computed } from "vue";
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { useTranslation } from 'i18next-vue';
+import { useHead } from '@vueuse/head';
 // dynamic title
 const { t } = useTranslation();
 useHead({
-  title: computed(() => t("appName") || "Family Accounting"),
+  title: t('appName'),
 });
-
 </script>
