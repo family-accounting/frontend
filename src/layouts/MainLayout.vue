8299275bd96ef1808da68146b05c627c4b02cf63
Amir Maghami <Maghami.A1987@gmail.com>
2025-10-21 11:11:09 +0330

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,59 +1,61 @@
 <template>
-  <IonMenu content-id="main-content">
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Menu Content</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent class="ion-padding">
-      <form>
-        <IonItem>
-          <IonToggle />
-        </IonItem>
-        <IonItem>
-          <IonRadioGroup value="fa" @change="handleLanguageChange($event)">
-            <IonRadio value="fa">fa</IonRadio><br />
-            <IonRadio value="en">en</IonRadio><br />
-          </IonRadioGroup>
-        </IonItem>
-        <IonItem>
-          <IonButton type="submit" expand="block">Save</IonButton>
-        </IonItem>
-      </form>
-    </IonContent>
-  </IonMenu>
-  <IonPage id="main-content">
-    <IonTabs>
-      <IonRouterOutlet :key="$route.fullPath"></IonRouterOutlet>
+  <div>
+    <IonMenu content-id="main-content">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Menu Content</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent class="ion-padding">
+        <form>
+          <IonItem>
+            <IonToggle />
+          </IonItem>
+          <IonItem>
+            <IonRadioGroup value="fa" @change="handleLanguageChange($event)">
+              <IonRadio value="fa">fa</IonRadio><br />
+              <IonRadio value="en">en</IonRadio><br />
+            </IonRadioGroup>
+          </IonItem>
+          <IonItem>
+            <IonButton type="submit" expand="block">Save</IonButton>
+          </IonItem>
+        </form>
+      </IonContent>
+    </IonMenu>
+    <IonPage id="main-content">
+      <IonTabs>
+        <IonRouterOutlet :key="$route.fullPath"></IonRouterOutlet>
 
-      <IonTabBar slot="bottom">
-        <IonTabButton tab="groups" href="/groups">
-          <IonIcon aria-hidden="true" :icon="people" />
-          <IonLabel>{{ t('groups') }}</IonLabel>
-        </IonTabButton>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="groups" href="/groups">
+            <IonIcon aria-hidden="true" :icon="people" />
+            <IonLabel>{{ t('groups') }}</IonLabel>
+          </IonTabButton>
 
-        <IonTabButton tab="wallets" href="/wallets">
-          <IonIcon aria-hidden="true" :icon="wallet" />
-          <IonLabel>{{ t('wallets') }}</IonLabel>
-        </IonTabButton>
+          <IonTabButton tab="wallets" href="/wallets">
+            <IonIcon aria-hidden="true" :icon="wallet" />
+            <IonLabel>{{ t('wallets') }}</IonLabel>
+          </IonTabButton>
 
-        <IonTabButton tab="accounts" href="/accounts">
-          <IonIcon aria-hidden="true" :icon="person" />
-          <IonLabel>{{ t('accounts') }}</IonLabel>
-        </IonTabButton>
+          <IonTabButton tab="accounts" href="/accounts">
+            <IonIcon aria-hidden="true" :icon="person" />
+            <IonLabel>{{ t('accounts') }}</IonLabel>
+          </IonTabButton>
 
-        <IonTabButton tab="transactions" href="/transactions">
-          <IonIcon aria-hidden="true" :icon="swapHorizontal" />
-          <IonLabel>{{ t('transactions') }}</IonLabel>
-        </IonTabButton>
+          <IonTabButton tab="transactions" href="/transactions">
+            <IonIcon aria-hidden="true" :icon="swapHorizontal" />
+            <IonLabel>{{ t('transactions') }}</IonLabel>
+          </IonTabButton>
 
-        <IonTabButton tab="reports" href="/reports">
-          <IonIcon aria-hidden="true" :icon="statsChart" />
-          <IonLabel>{{ t('reports') }}</IonLabel>
-        </IonTabButton>
-      </IonTabBar>
-    </IonTabs>
-  </IonPage>
+          <IonTabButton tab="reports" href="/reports">
+            <IonIcon aria-hidden="true" :icon="statsChart" />
+            <IonLabel>{{ t('reports') }}</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonPage>
+  </div>
 </template>
 
 <script setup lang="ts">
